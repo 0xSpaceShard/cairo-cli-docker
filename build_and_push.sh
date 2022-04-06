b6164a6d0c864423dd2a8960ebd3eded4d631e0b
FabijanC <fabijan.corak@gmail.com>
2022-04-07 00:43:04 +0200

    Refactor build_and_push.sh

--- build_and_push.sh
+++ build_and_push.sh
@@ -2,35 +2,33 @@
 set -e
 
 # idea from: https://stackoverflow.com/a/50945459
-# returns "yes" or "no" instead of exit code so as to be able to use the benefits of `set -e`
 function docker_tag_exists() {
-    curl --silent -f -lSL https://hub.docker.com/v2/repositories/$1/tags/$2 > /dev/null 2>&1 && echo "yes" || echo "no"
+    curl --silent -f -lSL https://hub.docker.com/v2/repositories/$1/tags/$2 > /dev/null 2>&1
 }
 
 IMAGE=shardlabs/cairo-cli
 VERSIONS_FILE=versions.txt
 
-curl https://pypi.org/pypi/cairo-lang/json | jq .releases | jq keys | sed -nr "s/^ *\"(.*)\".*/\1/p" > $VERSIONS_FILE
+curl https://pypi.org/pypi/cairo-lang/json | jq ".releases | keys" | sed -nr "s/^ *\"(.*)\".*/\1/p" > $VERSIONS_FILE
 
 docker login --username $DOCKER_USER --password $DOCKER_PASS
-while read VERSION; do
-    if [ -z $VERSION ]; then
+while read version; do
+    if [ -z $version ]; then
         continue
     fi
 
-    echo "Version: $VERSION"
+    echo "Version: $version"
 
-    if [ $(docker_tag_exists $IMAGE $VERSION) = "yes" ]; then
-        echo "Skipping"
-        echo
+    if docker_tag_exists $IMAGE $version ; then
+        printf "Skipping\n\n"
         continue
     fi
 
-    REQUIREMENTS_URL="https://raw.githubusercontent.com/starkware-libs/cairo-lang/v$VERSION/scripts/requirements.txt"
+    REQUIREMENTS_URL="https://raw.githubusercontent.com/starkware-libs/cairo-lang/v$version/scripts/requirements.txt"
     wget "$REQUIREMENTS_URL" -O requirements.txt
 
-    TAGGED_IMAGE=$IMAGE:$VERSION
-    docker build -t $TAGGED_IMAGE --build-arg CAIRO_VERSION=$VERSION .
+    TAGGED_IMAGE=$IMAGE:$version
+    docker build -t $TAGGED_IMAGE --build-arg CAIRO_VERSION=$version .
     docker push $TAGGED_IMAGE
 done < $VERSIONS_FILE
 

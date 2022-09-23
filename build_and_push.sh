2ee72730a64684f4701276ba9e4df615ffa73309
FabijanC <fabijan.corak@gmail.com>
2022-09-23 09:58:42 +0200

    Always build only the specified cairo-lang and push

--- build_and_push.sh
+++ build_and_push.sh
@@ -1,77 +1,31 @@
 #!/bin/bash
-set -e
-
-# idea from: https://stackoverflow.com/a/50945459
-function docker_tag_exists() {
-    curl --silent -f -lSL https://hub.docker.com/v2/repositories/$1/tags/$2 > /dev/null 2>&1
-}
+set -eu
 
 IMAGE=shardlabs/cairo-cli
-VERSIONS_FILE=versions.txt
+TAG="${CAIRO_VERSION}${TAG_SUFFIX}"
 
-curl https://pypi.org/pypi/cairo-lang/json \
-| jq ".releases | keys" \
-| sed -nr "s/^ *\"(.*)\".*/\1/p" \
-| sort -t "." -k1,1n -k2,2n -k3,3n \
-> $VERSIONS_FILE
+docker login --username "$DOCKER_USER" --password "$DOCKER_PASS"
 
-
-docker login --username $DOCKER_USER --password $DOCKER_PASS
-
-# Will iterate over sorted versions and start building images when minimum version reached
-if [ -z $MIN_VER ]; then
-    MIN_VER_REACHED="true"
-else
-    MIN_VER_REACHED="false"
+# get requirements
+REQUIREMENTS_URL="https://raw.githubusercontent.com/starkware-libs/cairo-lang/v$version/scripts/requirements.txt"
+status=$(curl -s -o /dev/null -w "%{http_code}" "$REQUIREMENTS_URL")
+if [ "$status" != 200 ]; then
+    echo "Error! Got status $status while fetching requirements"
+    exit 1
 fi
+curl "$REQUIREMENTS_URL" > requirements.txt
 
-while read version; do
-    if [ -z $version ]; then
-        continue
-    fi
+# build and tag
+TAGGED_IMAGE="$IMAGE:$TAG"
+LATEST_IMAGE="$IMAGE:latest$TAG_SUFFIX"
+docker build \
+    -t "$TAGGED_IMAGE" -t "$LATEST_TAG" \
+    --build-arg CAIRO_VERSION="$CAIRO_VERSION" .
 
-    tag="${version}${TAG_SUFFIX}"
-    echo "Version: $version; Tag: $tag"
+# verify
+docker run "$TAGGED_IMAGE" starknet-compile --version
+docker run "$TAGGED_IMAGE" starknet --version
 
-    # if at least at minimum version, proceed with building
-    if [ $MIN_VER_REACHED == "false" ]; then
-        if [ $version == "$MIN_VER" ]; then
-            MIN_VER_REACHED="true"
-        else
-            printf "Minimum not reached. Skipping\n\n"
-            continue
-        fi
-    fi
-
-    if docker_tag_exists $IMAGE $tag ; then
-        printf "Image exists. Skipping\n\n"
-        continue
-    fi
-
-    REQUIREMENTS_URL="https://raw.githubusercontent.com/starkware-libs/cairo-lang/v$version/scripts/requirements.txt"
-    status=$(curl -s -o /dev/null -w "%{http_code}" "$REQUIREMENTS_URL")
-    if [ "$status" != 200 ]; then
-        printf "Warning! Got status $status\n\n"
-        continue
-    fi
-    curl "$REQUIREMENTS_URL" > requirements.txt
-
-    TAGGED_IMAGE=$IMAGE:$tag
-    docker build -t $TAGGED_IMAGE --build-arg CAIRO_VERSION=$version .
-
-    # verify
-    docker run $TAGGED_IMAGE starknet-compile --version
-    docker run $TAGGED_IMAGE starknet --version
-
-    docker push $TAGGED_IMAGE
-done < $VERSIONS_FILE
-
-NEWEST=$(tail -n 1 $VERSIONS_FILE)
-NEWEST_TAG="${NEWEST}${TAG_SUFFIX}"
-if [[ "$(docker images -q $IMAGE:$NEWEST_TAG 2> /dev/null)" == "" ]]; then
-    echo "The newest version ($NEWEST_TAG) has already been pushed"
-else
-    LATEST_TAG="latest$TAG_SUFFIX"
-    docker tag $IMAGE:$NEWEST_TAG $IMAGE:$LATEST_TAG
-    docker push $IMAGE:$LATEST_TAG
-fi
+# push
+docker push "$TAGGED_IMAGE"
+docker push "$LATEST_IMAGE"

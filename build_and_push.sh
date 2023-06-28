c0b97d630d63ce2722d8753674165bd184070b9f
Nathan-SL <nl@shardlabs.io>
2023-06-28 11:49:59 +0300

    Update config and script

--- build_and_push.sh
+++ build_and_push.sh
@@ -38,14 +38,21 @@ cairo_0_output="$dir/artifacts/contracts/cairo"
 mkdir -p "$cairo_1_output"
 mkdir -p "$cairo_0_output"
 
-docker run -v "$dir":"$dir" "$TAGGED_IMAGE" sh -c "starknet --version \
+docker run "$TAGGED_IMAGE" sh -c "starknet --version \
     && starknet-compile-deprecated --version \
     && /usr/local/bin/target/release/starknet-compile --version \
-    && /usr/local/bin/target/release/starknet-sierra-compile --version \
-    && starknet-compile-deprecated "$dir/$cairo_0_contract" --abi "$cairo_0_output/contract_abi.json" --output "$cairo_0_output/contract.json" \
-    && /usr/local/bin/target/release/starknet-compile "$dir/$cairo_1_contract" "$cairo_1_output/contract1.json" \
+    && /usr/local/bin/target/release/starknet-sierra-compile --version"
+
+# compile cairo 0 contract
+docker run -v "$dir":"$dir" "$TAGGED_IMAGE" \
+    sh -c "starknet-compile-deprecated "$dir/$cairo_0_contract" --abi "$cairo_0_output/contract_abi.json" --output "$cairo_0_output/contract.json""
+
+# compile cairo 1 contract
+docker run -v "$dir":"$dir" "$TAGGED_IMAGE" \
+    sh -c  "/usr/local/bin/target/release/starknet-compile "$dir/$cairo_1_contract" "$cairo_1_output/contract1.json" \
     && /usr/local/bin/target/release/starknet-sierra-compile "$cairo_1_output/contract1.json" "$cairo_1_output/contract1.casm""
 
+# verify compilation
 if [[ ! -f "$cairo_0_output/contract.json"  ||  ! -f "$cairo_0_output/contract_abi.json" ]]; then
     echo "One or more Cairo 0 files not found!"
     exit 1
